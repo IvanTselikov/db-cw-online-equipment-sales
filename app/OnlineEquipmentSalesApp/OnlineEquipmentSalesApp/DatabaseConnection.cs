6fe64408d31263555db21ed6d3f2e7471f1deb40
IvanTselikov <ivantselikov@yandex.ru>
2022-12-23 23:24:53 +0300

    Инициализация приложения

---
+++ app/OnlineEquipmentSalesApp/OnlineEquipmentSalesApp/DatabaseConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient; // NuGet Microsoft.Data.SqlClient
+
+namespace OnlineEquipmentSalesApp
+{
+    class DatabaseConnection
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=LAPTOP-J4FHUBL5\SQLEXPRESS;" +
+                                                         "Initial Catalog:OnlineEquipmentSales;" +
+                                                         "Integrated Security=True");
+
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
