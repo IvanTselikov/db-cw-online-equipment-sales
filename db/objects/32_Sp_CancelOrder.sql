52c9c71df7e4277643014710e8ba111c37fa047a
IvanTselikov <ivantselikov@yandex.ru>
2022-12-26 12:44:43 +0300

    Исправлена ХП отмены заказа

--- db/objects/32_Sp_CancelOrder.sql
+++ db/objects/32_Sp_CancelOrder.sql
@@ -15,7 +15,7 @@ AS
   END;
   ELSE IF @currentOrderStatus IN (10, 11, 12)
   BEGIN
-    RAISERROR('Нельзя отменить полученный заказ или заказ, готовый к выдаче!', 16, 10);
+    RAISERROR('Нельзя отменить заказ, готовый к выдаче, полученный или ранее отменённый!', 16, 10);
     RETURN 1;
   END;
   ELSE IF @currentOrderStatus NOT IN (SELECT code FROM OrderStatuses)
