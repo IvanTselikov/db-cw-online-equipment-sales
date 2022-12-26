c1e71302e91c795bcee45850d2d1738cb7cb3b94
IvanTselikov <ivantselikov@yandex.ru>
2022-12-26 21:00:40 +0300

    Реализована поддержка любых серверов при запуске сценария

--- db/fill.bat
+++ db/fill.bat
@@ -1,3 +1,10 @@
-cd objects
-for %%G in (*.sql) do sqlcmd /S LAPTOP-J4FHUBL5\SQLEXPRESS /d OnlineEquipmentSales -E -i"%%G"
+if "%1"=="" (
+    sqlcmd -E -S LAPTOP-J4FHUBL5\SQLEXPRESS -Q "RESTORE DATABASE OnlineEquipmentSales FROM DISK='OnlineEquipmentSales.bak'"
+    cd objects
+    for %%G in (*.sql) do sqlcmd /S LAPTOP-J4FHUBL5\SQLEXPRESS /d OnlineEquipmentSales -E -i"%%G"
+) else (
+    sqlcmd -E -S %1 -Q "RESTORE DATABASE OnlineEquipmentSales FROM DISK='OnlineEquipmentSales.bak'"
+    cd objects
+    for %%G in (*.sql) do sqlcmd /S %1 /d OnlineEquipmentSales -E -i"%%G"
+)
 pause
