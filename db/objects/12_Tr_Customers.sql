04a15ab6c591b6bc25075dc7cfc23ddd40234047
IvanTselikov <ivantselikov@yandex.ru>
2022-12-26 16:21:03 +0300

    Исправлен триггер 12

--- db/objects/12_Tr_Customers.sql
+++ db/objects/12_Tr_Customers.sql
@@ -3,14 +3,14 @@ GO
 
 CREATE TRIGGER tr_CheckCustomerBirthDate
 ON Customers
-INSTEAD OF UPDATE, INSERT 
+AFTER UPDATE, INSERT 
 AS
 	DECLARE @dateOfB DATE	
 	SELECT @dateOfB = dateOfBirth 
 	FROM inserted
 
 	IF @dateOfB >= CAST(GETDATE() AS DATE)
-		RAISERROR('ƒата рождени€ должна быть меньше текущей даты', 16, 1)
-
-	INSERT INTO Customers
-	SELECT firstName, secondName, patronymic, dateOfBirth, phoneNumber, sex, email FROM inserted
+	BEGIN
+		RAISERROR('ƒата рождени€ должна быть меньше текущей даты', 16, 1);
+		ROLLBACK TRAN;
+	END
