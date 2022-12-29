dae3da1392278fd23e1ca54bed920313c2539eee
IvanTselikov <ivantselikov@yandex.ru>
2022-12-29 11:30:27 +0300

    Разрешена отмена заказов, готовых к выдаче

--- db/objects/32_Sp_CancelOrder.sql
+++ db/objects/32_Sp_CancelOrder.sql
@@ -13,9 +13,9 @@ AS
     RAISERROR('Заказ не существует!', 16, 10);
     RETURN 1;
   END;
-  ELSE IF @currentOrderStatus IN (10, 11, 12)
+  ELSE IF @currentOrderStatus IN (11, 12)
   BEGIN
-    RAISERROR('Нельзя отменить заказ, готовый к выдаче, полученный или ранее отменённый!', 16, 10);
+    RAISERROR('Нельзя отменить полученный или ранее отменённый заказ!', 16, 10);
     RETURN 1;
   END;
   ELSE IF @currentOrderStatus NOT IN (SELECT code FROM OrderStatuses)
